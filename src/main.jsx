412975cff4ad78be2a4922564c5e46b4ca2b6d8d
Sadik <121496253+Sadik-Al-Sami@users.noreply.github.com>
2023-08-04 16:36:09 +0600

    Nav bar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Home from './components/Home/Home'
+import About from './components/About/About'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Books from './components/Books/Books'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "books",
+                element: <Books />
+            },
+            {
+                path: "about",
+                element: <About />
+            }
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
